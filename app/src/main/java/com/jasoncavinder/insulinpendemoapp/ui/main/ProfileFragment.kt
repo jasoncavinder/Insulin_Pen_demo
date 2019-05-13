433a7ee8b7c6dcd6d826e73076d7a34acf4ef4ae
Jason Cavinder <jason.cavinder@gmail.com>
2019-05-13 06:02:17 -1000

    Merge remote-tracking branch 'origin/master'
    Signed-off-by: Jason Cavinder <jason.cavinder@gmail.com>

--- app/src/main/java/com/jasoncavinder/insulinpendemoapp/ui/main/ProfileFragment.kt
+++ app/src/main/java/com/jasoncavinder/insulinpendemoapp/ui/main/ProfileFragment.kt
@@ -14,6 +14,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavController
 import com.jasoncavinder.insulinpendemoapp.R
@@ -83,6 +84,7 @@ class ProfileFragment : Fragment() {
             )
         )
 
+        mainViewModel.userProfile.observe(this, Observer { })
     }
 
 
